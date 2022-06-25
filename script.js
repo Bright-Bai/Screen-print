3c9a1234cfe4f5bcdba163e22623b9381689fd31
Bright-Bai <96099415+Bright-Bai@users.noreply.github.com>
2022-06-25 15:26:38 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let fullName = "Ediabai Odeleye";
+let height = "167cm";
+let country = "Nigeria";
+
+alert(`My name is ${fullName}.`);
+alert(`I am ${height} tall.`);
+alert(`I am from ${country}.`);
